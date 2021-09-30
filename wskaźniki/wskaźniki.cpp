dcbf43032ccb6b710ade93af0a5bbc92ba45ae0d
mariusz <mariusz@ADMIN>
2021-09-30 10:10:22 +0200

    krok2

--- wskaźniki/wskaźniki.cpp
+++ wskaźniki/wskaźniki.cpp
@@ -3,9 +3,23 @@ using namespace std;
 
 int main()
 {
+    int* wsk;
+
+    int a = 10;
+
+    wsk = &a; //przypisanie adresu komorki pamięci zmiennej a do wskaźnika wsk
+              // &a powoduje podanie adresu komorki pamieci zmiennej a
     
+    cout << "zmienna wyswietlona przez wskaźnik " << *wsk<<endl; //wyświetenie zawartości
+                // zmiennej a poprzez adres przechowywany we wskaźniku wsk
+    cout << "wyświetlenie adresu przechowywanego w wskaźniku " << wsk<<endl; // !!! jeżeli
+        //chcemy wyświetlic adres to bez gwiazdki
+    cout << "adres komórki pamieci gdzie jest przechowywana zmienna a " << &a << endl; // !!! jeżeli
+        //chcemy wyświetlic adres to bez gwiazdki
 
+    *wsk = 20;
 
+    cout << "nowa wartosc: " << a;
 
 }
 
